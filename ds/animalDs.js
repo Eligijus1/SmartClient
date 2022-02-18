7267a91f9f1eab61769171fec978db5de422751d
Eligijus Stugys <eligijus.stugys@real-digital.de>
2022-02-18 17:28:57 +0200

    Adding some test data.

---
+++ ds/animalDs.js
@@ -0,0 +1,14 @@
+let animalDs = isc.DataSource.create({
+    ID: "animalDs",
+    dataFormat: "json",
+    dataURL: "/ds/data/animalData.json",
+    fields: [
+        {name: "commonName", hidden: true, title: "Animal"},
+        {name: "scientificName", title: "Scientific Name"},
+        // Note: Fields not included in this list will not show up in the ListGrid
+        //{name:"lifeSpan", title:"Life Span"},
+        //{name:"status",title:"Endangered Species Status"}
+        {name: "diet", title: "Diet"},
+        {name: "information", title: "Interesting Facts"}
+    ]
+});
---
+++ tools/win_variables.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set projectRoot=C:\Projects\SmartClient\
+set composerLocation=C:\Programs\tools\composer.phar
+
+
